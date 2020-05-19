eebd7b84c3a1b5b485e0c89e97a8556d294c5ada
Pavittar Singh <singh.pavittar95@icloud.com>
2020-05-20 00:42:14 +0530

    logo added meta added

--- components/header.js
+++ components/header.js
@@ -7,6 +7,39 @@ export default function Header() {
       <Head>
         <title>Pavittar Singh | Front end developer | Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Hello, my name is Pavittar Singh and I'm a front end developer in
+          India. My portfolio is a representation of all that I've learned
+          and accomplished as a FrontEnd Developer."
+        />
+        <meta
+          name="og:title"
+          content="Pavittar Singh | Front end developer | Portfolio"
+        />
+        <meta
+          name="og:description"
+          content="Hello, my name is Pavittar Singh and I'm a front end developer in
+          India. My portfolio is a representation of all that I've learned
+          and accomplished as a FrontEnd Developer."
+        />
+        <meta name="og:image" content="/logo.png" />
+        <meta
+          name="keywords"
+          content="Frontend developer, Portfolio, Developer, web developer, web design, web design solutions, react developer, angular developer, JS developer, Javascript developer, Designer, mern stack developer, mean stack developer, bootstrap, html, css"
+        />
+        <meta
+          name="subject"
+          content="Pavittar Singh | Front end developer | Portfolio"
+        />
+        <meta name="copyright" content="Pavittar Singh" />
+        <meta name="language" content="ES" />
+        <meta name="hostname" content="portfolio-coral-one.now.sh/" />
+        <meta name="expected-hostname" content="portfolio-coral-one.now.sh/" />
+        <meta name="revisit-after" content="7 days" />
+        <meta http-equiv="Expires" content="0" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Cache-Control" content="no-cache" />
       </Head>
       <h1 className="brand open-sans-font"> Pavittar Singh </h1>
       <ul className="links">
